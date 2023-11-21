31c383499b823c0485b7b7bb275a1f1ef77bea93
Jimbo Cortes <jimbo.cortes@gmail.com>
2023-11-21 08:04:11 +0800

    - Add dashboard layout, sidebar and logo

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+const Logo = () => {
+  return <Image src="/logo.svg" alt="logo" width={130} height={130} />;
+};
+
+export default Logo;
